6bb787dfe54049b4e9a7da926baa787ceed79dc8
growthdev1 <growthdev1@gmail.com>
2025-07-17 23:59:55 +0530

    chnage for admin screens

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/admin', require('./routes/admin'));
 
 // Health check route
 app.get('/api/health', (req, res) => {
